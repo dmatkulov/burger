3ceab7bd175d386c32e19ab71d684a51f5c8a3ae
Dilshad Matkulov <dilshadmatkulov@gmail.com>
2023-11-03 22:44:48 +0600

    replaced useMemo with reduce method

--- src/container/App.tsx
+++ src/container/App.tsx
@@ -1,84 +1,83 @@
-import {useMemo, useState} from 'react';
+import {useState} from 'react';
 import Burger from '../components/Burger/Burger';
 import {INGREDIENTS, Ingredients} from '../Types/types';
-import Menu from "../components/Menu/Menu";
-import Total from "../components/Total/Total";
+import Menu from '../components/Menu/Menu';
+import Total from '../components/Total/Total';
 
-import './App.css';
 
 import meatImage from '../assets/meat.png';
 import baconImage from '../assets/bacon.png';
 import cheeseImage from '../assets/cheese.png';
 import lettuceImage from '../assets/lettuce.png';
 
+import './App.css';
+
 const INGREDIENTS: INGREDIENTS[] = [
-    {name: 'Meat', price: 80, image: meatImage},
-    {name: 'Cheese', price: 50, image: cheeseImage},
-    {name: 'Salad', price: 10, image: lettuceImage},
-    {name: 'Bacon', price: 60, image: baconImage},
+  {name: 'Meat', price: 80, image: meatImage},
+  {name: 'Cheese', price: 50, image: cheeseImage},
+  {name: 'Salad', price: 10, image: lettuceImage},
+  {name: 'Bacon', price: 60, image: baconImage},
 ];
 
 const App = () => {
-    const [ingredients, setIngredients] = useState<Ingredients[]>([
-        {name: 'Meat', count: 0},
-        {name: 'Cheese', count: 0},
-        {name: 'Salad', count: 0},
-        {name: 'Bacon', count: 0},
-    ]);
+  const [ingredients, setIngredients] = useState<Ingredients[]>([
+    {name: 'Meat', count: 0},
+    {name: 'Cheese', count: 0},
+    {name: 'Salad', count: 0},
+    {name: 'Bacon', count: 0},
+  ]);
 
-    const addIngredient = (index: number) => {
-        setIngredients((prevState) => {
-            return prevState.map((ingredient, i) => {
-                if (index === i) {
-                    return {...ingredient, count: ingredient.count + 1};
-                }
-                return ingredient;
-            });
-        });
-    };
-
-    const getClasses = () => {
-      return ingredients.reduce((acc: string[], item) => {
-        if (item.count > 0) {
-          for (let i = 0; i < item.count; i++) {
-            acc.push(item.name);
-          }
+  const addIngredient = (index: number) => {
+    setIngredients((prevState) => {
+      return prevState.map((ingredient, i) => {
+        if (index === i) {
+          return {...ingredient, count: ingredient.count + 1};
         }
-        return acc;
-      }, []);
-    };
-    const removeIngredient = (index: number) => {
-        setIngredients((prevState) => {
-            return prevState.map((ingredient, i) => {
-                if (index === i && ingredient.count > 0) {
-                    return {...ingredient, count: ingredient.count - 1};
-                }
-                return ingredient;
-            });
-        });
-    };
+        return ingredient;
+      });
+    });
+  };
 
-    const total = useMemo(() => {
-        return INGREDIENTS.reduce((total, ingredient) => {
-            const selectedIngredients = ingredients.filter((i) => i.name === ingredient.name);
-            const ingredientCount = selectedIngredients.length > 0 ? selectedIngredients[0].count : 0;
-            return total + ingredientCount * ingredient.price;
-        }, 30);
-    }, [ingredients]);
+  const getClasses = () => {
+    return ingredients.reduce((acc: string[], item) => {
+      if (item.count > 0) {
+        for (let i = 0; i < item.count; i++) {
+          acc.push(item.name);
+        }
+      }
+      return acc;
+    }, []);
+  };
+  const removeIngredient = (index: number) => {
+    setIngredients((prevState) => {
+      return prevState.map((ingredient, i) => {
+        if (index === i && ingredient.count > 0) {
+          return {...ingredient, count: ingredient.count - 1};
+        }
+        return ingredient;
+      });
+    });
+  };
 
-    return (
-        <div className="Application">
-                <Menu
-                    INGREDIENTS={INGREDIENTS}
-                    ingredients={ingredients}
-                    onAdd={(index) => addIngredient(index)}
-                    onRemove={(index) => removeIngredient(index)}/>
-            <div className="col">
-                <Burger ingredientClasses={getClasses()}/>
-                <Total count={total}/>
-            </div>
-        </div>
-    );
+  const total = INGREDIENTS.reduce((total, ingredient) => {
+    const ingredientsCount = ingredients.filter((i) => i.name === ingredient.name);
+    const count = ingredientsCount.length > 0 ? ingredientsCount[0].count : 0;
+    return total + (count * ingredient.price);
+  }, 30);
+
+  return (
+    <div className="Application">
+      <Menu
+        INGREDIENTS={INGREDIENTS}
+        ingredients={ingredients}
+        onAdd={(index) => addIngredient(index)}
+        onRemove={(index) => removeIngredient(index)}/>
+      <div className="col">
+        <Burger ingredientClasses={getClasses()}/>
+        <Total count={total}/>
+      </div>
+    </div>
+  );
 };
 
 export default App;
